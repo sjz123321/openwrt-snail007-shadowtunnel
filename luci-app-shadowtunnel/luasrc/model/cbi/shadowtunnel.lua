875f3bdf960857c194b3ac160c9524e37fb8af6d
shijiezuan <shijiezuan@gmail.com>
2019-07-01 11:26:27 +0800

    Signed-off-by: shijiezuan <shijiezuan@gmail.com>

--- luci-app-shadowtunnel/luasrc/model/cbi/shadowtunnel.lua
+++ luci-app-shadowtunnel/luasrc/model/cbi/shadowtunnel.lua
@@ -11,9 +11,10 @@ switch:value("enable",translate("enable"))
 switch:value("disable",translate("disable"))
 
 mode=s:option(ListValue,"mode",translate("Proxy mode selection"))
-mode:value("GFW",translate("Bypassing the Chinese mainland's ip mode"))
+mode:value("CHN_ONLY",translate("Proxy Chinese IP Only"))
 mode:value("Global",translate("Global mode"))
 mode:value("DNS",translate("DNS mode"))
+mode:value("GFW",translate("Bypassing the Chinese mainland's ip mode"))
 
 ipaddr=s:option(DynamicList,"ipaddr",translate("server ip address"))
 ipaddr.description = translate("Note: Please enter the server's password@ip:port#weight like example. Example st@11.22.33.44:5566#1 Both @.. and #.. is optional. a. Using weight balance-strategy ...#(your_setting_weight). b. Setting unique password for this server (your_passwd)@... ps. -p password is a global password. if you are not setting unique password for the server")
@@ -35,7 +36,6 @@ encrypt:value("rc4-md5")
 encrypt:value("chacha20")
 
 passwd=s:option(Value,"passwd",translate("Global Password"))
-passwd.datatype = "host"
 passwd.rmempty = true
 
 
@@ -47,7 +47,6 @@ udp.description = translate("Note: To save memory , compression is off")
 dns_enable=s:option(ListValue,"dns_enable",translate("prevent DNS pollution"))
 dns_enable:value("1",translate("enable"))
 dns_enable:value("0",translate("disable"))
-dns_enable:depends({mode="GFW"})
 dns_enable:depends({mode="Global"})
 
 strategy=s:option(ListValue,"strategy",translate("balance strategy"))
@@ -86,6 +85,8 @@ hour:value("24",translate("disable"))
 hour.description = translate("Note:To ensure GFW is up to date GFW-List will be update every month.but it may cost a long time so it is recommanded to update when you are not using router.")
 hour:depends({mode="GFW"})
 hour:depends({mode="Global"})
+hour:depends({mode="CHN_ONLY"})
+
 
 reset=s:option(ListValue,"reset",translate("Automaticly restart shadowtunnel"))
 reset:value("0",translate("disable"))
@@ -103,13 +104,17 @@ hosts=s:option(TextValue,"hosts",translate("Custom hosts setting"))
 hosts.rmempty = true   
 hosts.rows = 10 
 hosts.description = translate("<br/>Note: Shadowtunnel provides custom hosts in the same format as the hosts file,which you can customize as needed")
-hosts:depends({mode="DNS"})                                                                           
+hosts:depends({mode="CHN_ONLY"}) 
+hosts:depends({mode="GFW"})
+hosts:depends({mode="DNS"})                                                                          
 hosts:depends({dns_enable="1"})
 
 dns_forward=s:option(TextValue,"dns_forward",translate("Custom Dns forward setting"))
 dns_forward.rmempty = true   
 dns_forward.rows = 10 
-dns_forward:depends({mode="DNS"})                                                                     
+dns_forward:depends({mode="CHN_ONLY"}) 
+dns_forward:depends({mode="GFW"})
+dns_forward:depends({mode="DNS"})                                                                    
 dns_forward:depends({dns_enable="1"})
 
 local e=luci.http.formvalue("cbi.apply")
